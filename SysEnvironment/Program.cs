af775d3f969ca7ece5910c918f3ec7f8a92f5441
陈志同 <chenzhitong12@hotmail.com>
2018-05-17 17:25:31 +0800

    initial commit

--- SysEnvironment/Program.cs
+++ SysEnvironment/Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SysEnvironment
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            SetPath(Environment.CurrentDirectory);
+            Console.ReadLine();
+        }
+        
+        public static void SetPath(string directory)
+        {
+            var userEnvironment  = Registry.CurrentUser.OpenSubKey("Environment", true);
+            string path = userEnvironment.GetValue("PATH").ToString();
+            if (!path.Split(';').Contains(directory))
+            {
+                userEnvironment.SetValue("PATH", path + directory + ";");
+                Console.WriteLine("已将当前目录添加到用户的环境变量PATH中");
+            }
+            else
+            {
+                Console.WriteLine("环境变量PATH中已存在当前目录，不用重复添加");
+            }
         }
     }
 }
